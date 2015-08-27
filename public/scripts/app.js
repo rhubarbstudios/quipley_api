7f0cb78e45caaecb7ce98d4d7beb573e1c30b0af
Jefferson Choi <jeffersonchoi@gmail.com>
2015-08-27 15:31:56 -0700

    preview modal screen working

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
       $("#amount-text").show();
       $("#percentage-text").hide();
       $("#total-field").show();
-      $("#total-field-value").text("$ ")
+      $(".total-field-value").text("$ ")
       $("#user-amount-input").val("")
     })
     $(document).on("click", '#percentage-label', function(){
@@ -15,17 +15,18 @@ $(document).ready(function () {
       $("#percentage-text").show();
       $("#amount-text").hide();
       $("#total-field").show();
-      $("#total-field-value").text(" %")
+      $(".total-field-value").text(" %")
       $("#user-percentage-input").val("")
     })
 
     $(document).on("keyup", "#user-amount-input", function() {
+      console.log("testing");
       var input = $("#user-amount-input").val();
-          $("#total-field-value").text("$"+input*1.25)
+          $(".total-field-value").text("$"+input*1.25)
     })
     $(document).on("keyup", "#user-percentage-input", function() {
       var input = $("#user-percentage-input").val();
-          $("#total-field-value").text(input*1.25+"%")
+          $(".total-field-value").text(input*1.25+"%")
     })
 
 
